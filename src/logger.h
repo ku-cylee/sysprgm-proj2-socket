7cf3b9e5a3d3029fee3efeb4c4e283cecce33994
2020sp-client <btstar_cy@korea.ac.kr>
2020-11-09 01:25:58 +0900

    Updated header files

--- src/logger.h
+++ src/logger.h
@@ -1,12 +1,3 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "errno.h"
-#include "time.h"
-#include "sys/timeb.h"
-#include "sys/stat.h"
-
 FILE *open_log(int fd, int port);
-
 void write_log(FILE *fp, int msg_size, char *buffer);
-
 void close_log(FILE *fp);
--- src/socket-manager.h
+++ src/socket-manager.h
@@ -1,20 +1,8 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "unistd.h"
 #include "arpa/inet.h"
-#include "sys/types.h"
-#include "sys/socket.h"
-#include "netinet/in.h"
 
 int atsign_counting(const char * const buf, size_t len);
-
 void read_from_server(int fd, int port);
-
 int connect_socket(int fd, int port, struct sockaddr_in *sv_addr);
-
 void set_server_addr(struct sockaddr_in *sv_addr, int port);
-
 int create_socket();
-
 void *socket_main(void *arg);
