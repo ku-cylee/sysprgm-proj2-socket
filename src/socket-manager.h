42edda5e0d27271a9f3ed5a6354e47eb360b5309
2020sp-client <btstar_cy@korea.ac.kr>
2020-11-09 01:50:27 +0900

    Removed unneccessary code in socket-manager.h

--- src/socket-manager.h
+++ src/socket-manager.h
@@ -1,8 +1,3 @@
 #include "arpa/inet.h"
 
-void count_end_sign(const char * const buf, size_t len, int *count);
-void read_from_server(int fd, int port);
-int connect_socket(int fd, int port, struct sockaddr_in *sv_addr);
-void set_server_addr(struct sockaddr_in *sv_addr, int port);
-int create_socket();
 void *socket_main(void *arg);
